c427679dbe00ce513e01e83126c4dadae3d338b9
James Curran <dzejms@gmail.com>
2019-05-27 09:23:54 -0400

    basic start/stop buttons wired up

--- src/encapsulate/src/app/tab1/tab1.page.ts
+++ src/encapsulate/src/app/tab1/tab1.page.ts
@@ -8,19 +8,24 @@ import * as Tone from 'tone';
 })
 export class Tab1Page implements OnInit {
 
-  ngOnInit(): void {
-    console.log('init');
-    const synth = new Tone.Synth().toMaster();
-    //synth.triggerAttack('C4');
-    const noise = new Tone.Noise('pink').toMaster();
-    noise.start();
-    noise.stop(5);
-  }
+  private noise = new Tone.Noise('pink').toMaster();
 
   constructor() {
     if (typeof Tone !== 'undefined') {
       console.log('tone loc');
     }
+  }
+
+  ngOnInit(): void {
+
+    console.log('init');
+  }
+  start(): void {
+    this.noise.start();
+  }
+
+  stop(): void {
+    this.noise.stop();
   }
 
 }
